966ff94bb4e88ff7235aec91cd3dd9cded5c39a0
vagrant <vagrant@homestead>
2019-11-21 03:14:09 +0000

    CCI unit tests setup

---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VuePageTransition from 'vue-page-transition';
+import App from '@/App.vue';
+
+const localVue = createLocalVue();
+
+Vue.use(Vuex);
+Vue.use(VuePageTransition);
+
+describe('App.vue', () => {
+    let wrapper;
+    let store;
+
+    beforeEach(() => {
+        store = new Vuex.Store({
+            actions: {
+                getScreenWidth: jest.fn(),
+            }
+        });
+        wrapper = shallowMount(App, {
+            localVue,
+            store,
+            stubs: ['router-link', 'router-view']
+        });
+    });
+
+    test('App is a Vue instance', () => {
+        expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+})
