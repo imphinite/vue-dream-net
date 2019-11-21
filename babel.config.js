5ef488649f984a12e4938c731710fb55d1769f48
Charles Wang <A01035164@ad.bcit.ca>
2019-11-20 20:11:11 -0800

    Packages

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+  "presets": [
+    "@babel/preset-env"
+  ],
+  "plugins": [
+      "transform-vue-jsx",
+      "@babel/plugin-transform-runtime",
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-proposal-object-rest-spread"
+  ],
+  "env": {
+      "test": {
+        "plugins": [
+          "transform-vue-jsx",
+          "@babel/plugin-transform-modules-commonjs",
+          "dynamic-import-node"
+        ]
+      }
+  },
 }
