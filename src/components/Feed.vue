16a40b0e0e1ac78dea4149548165b39b1ee973a0
ImPhinite <untildeathdousapar@gmail.com>
2019-12-05 22:45:26 -0800

    WIP: posts

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,27 +1,20 @@
 <template>
-    <v-card dark>
-        <v-list two-line>
-            <v-subheader
-                v-if="subheader"
-                :key="subheader"
-            >
-                {{ subheader }}
-            </v-subheader>
+    <v-card>
+        <v-subheader
+            v-if="subheader"
+            :key="subheader"
+        >
+            {{ subheader }}
+        </v-subheader>
+        <v-list v-if="feed.data" two-line>
             <template v-for="(post, index) in feed.data">
-                <v-list-tile
+                <Post
                     :key="index"
-                    avatar
-                >
-                    <v-list-tile-avatar>
-                        <img :src="imageSrc(post.id)">
-                    </v-list-tile-avatar>
-
-                    <v-list-tile-content>
-                        <v-list-tile-title v-html="post.content" />
-                        <v-list-tile-sub-title v-html="post.subtitle" />
-                    </v-list-tile-content>
-                    <v-divider :inset="true" />
-                </v-list-tile>
+                    :id="post.id"
+                    :content="post.content"
+                    :author="post.author"
+                    :date="post.created_at"
+                />
             </template>
         </v-list>
     </v-card>
@@ -29,9 +22,13 @@
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import Post from '@/components/Post';
 
 export default {
     name: 'Feed',
+    components: {
+        Post
+    },
     data () {
         return {
             subheader: 'Today'
@@ -46,10 +43,7 @@ export default {
     methods: {
         ...mapActions('feed', [
             'getDreamPosts'
-        ]),
-        imageSrc (id) {
-            return `https://i.pravatar.cc/300?u=${id}`;
-        }
+        ])
     }
 }
 </script>
---
+++ src/components/Post.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-list-tile
+        avatar
+    >
+        <v-list-tile-avatar>
+            <img :src="imageSrc">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+            {{ post.content }}
+        </v-list-tile-content>
+    </v-list-tile>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+    props: {
+        id: {
+            type: Number,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: Object,
+            default: () => {
+                return {
+                    name: 'anonymous',
+                    avatar: `https://i.pravatar.cc/300?u=anonymous@dnet.com`
+                }
+            }
+        },
+        date: {
+            type: Date,
+            required: true
+        }
+    },
+    computed: {
+        imageSrc () {
+            return `https://i.pravatar.cc/300?u=${this.id}`;
+        }
+    }
+}
+</script>
